6eb80a40c3a4b12cc841607598711377d6c22352
K1681 <mohammedkhalidammam@gmail.com>
2025-04-16 22:20:11 +0530

    Epoching complete

--- main.py
+++ main.py
@@ -15,38 +15,55 @@ def notch_filter(data, fn, fs, q):
 
 def main():
     # Constants
-    T = 5492
-    fs = 256
-    fl = 1
-    fh = 80
-    fn = 60
-    q = 100
-    no_channels = 214
-    feeling_names = ["awe", "frustration", "joy", "anger", "happy", "sad", "love", "grief", "fear", "compassion", "jealousy", "content", "grief", "relief", "excite", "disgust", "relax"]
-    N = len(feeling_names)
-
-    # Reading EEG data
+    T = 5492  # Recording time (seconds) of each user.
+    fs = 256  # Rampling rate (Hz)
+    fl = 1    # Lower frequency of band pass filer
+    fh = 80   # Higher frequency of band pass filter
+    fn = 60   # Line freequency, to be removed by notch filter
+    q = 100   # Quality factor of the notch filter
+    no_channels = 214  # Number of channels
+    baseline_name = "relax"
+    baseline_end_name = "ImaginationSuggestions"
+    emotion_names = ["awe", "frustration", "joy", "anger", "happy", "sad", "love", "grief", "fear", "compassion", "jealousy", "content", "grief", "relief", "excite", "disgust"]
+    emotion_end_name = "exit"
+    emotions_file_name = "sub-02_task-ImaginedEmotion_events.txt"
     data_file_name = "sub-02_task-ImaginedEmotion_eeg.set"
-    raw = mne.io.read_raw_eeglab(f"./sources/{data_file_name}")
-    data, times = raw.get_data(return_times=True, units='uV')
 
-    # Reading annotations
+    # Reading baseline annotations
+    # N = len(emotion_names)  # Number of emotions to detect
     T_max = 0
-    feeling_times = np.zeros((N, 3))
-    feelings_file_name = "sub-02_task-ImaginedEmotion_events.txt"
-    with open(f"sources/{feelings_file_name}", 'r') as text_file:
+    baseline_times = np.zeros(2)
+    with open(f"sources/{emotions_file_name}", 'r') as text_file:
+        tsv_values = list(csv.reader(text_file, delimiter="\t"))
+        for row in range(len(tsv_values)):
+            if(tsv_values[row][6].strip() == baseline_name):
+                baseline_times[0] = float(tsv_values[row][0].strip())
+                while(tsv_values[row][6].strip() != baseline_end_name):
+                    row += 1
+                baseline_times[1] = float(tsv_values[row][0].strip())
+    T_max = baseline_times[1] - baseline_times[0]
+
+    # Reading emotion annotations
+    emotion_times = []
+    with open(f"sources/{emotions_file_name}", 'r') as text_file:
         tsv_values = list(csv.reader(text_file, delimiter="\t"))
         counter = 0
         for row in range(len(tsv_values)):
-            if(tsv_values[row][6].strip() in feeling_names):
-                feeling_times[counter][0] = feeling_names.index(tsv_values[row][6].strip())
-                feeling_times[counter][1] = float(tsv_values[row][0].strip())
-                while(tsv_values[row][6].strip() != "exit"):
+            if(tsv_values[row][6].strip() in emotion_names):
+                emotion_times.append([0, 0, 0])
+                emotion_times[counter][0] = emotion_names.index(tsv_values[row][6].strip())
+                emotion_times[counter][1] = float(tsv_values[row][0].strip())
+                while(tsv_values[row][6].strip() != emotion_end_name):
                     row += 1
-                feeling_times[counter][2] = float(tsv_values[row][0].strip())
+                emotion_times[counter][2] = float(tsv_values[row][0].strip())
+                if((emotion_times[counter][2] - emotion_times[counter][1]) > T_max):
+                    T_max = emotion_times[counter][2] - emotion_times[counter][1]
                 counter += 1
-                if((feeling_times[counter][2] - feeling_times[counter][1]) > T_max):
-                    T_max = feeling_times[counter][2] - feeling_times[counter][1]
+    emotion_times = np.asarray(emotion_times, dtype=np.float32)
+
+    # Reading EEG data
+    raw = mne.io.read_raw_eeglab(f"./sources/{data_file_name}")
+    data, times = raw.get_data(return_times=True, units='uV')
 
     # Filtering data  # TODO: consolidate into one matrix
     bp_data = np.zeros((no_channels, T*fs))
@@ -56,15 +73,20 @@ def main():
         filtered_data[channel] = notch_filter(bp_data[channel], fn, fs, q)
 
     # Epoching data
-    epoched_data = np.zeros((no_channels, N, T_max*fs))
-    for channel in range(no_channels):
-        for feeling in range(N):
-            start, end = feeling_times[feeling_times[:].index(feeling)][1:2]  # TODO: fix searching.
-            epoched_data[channel][feeling] = filtered_data[channel][start : end ]
+    epoched_data = np.zeros((no_channels, len(emotion_times), int(T_max*fs)))
+    # epoched_data = [[] for _ in range(no_channels)]
+    for emotion_no in range(len(emotion_times)):
+        start_index, = np.where(np.isclose(times, emotion_times[emotion_no][1]))
+        end_index, = np.where(np.isclose(times, emotion_times[emotion_no][2]))
+        start_index = start_index[0]
+        end_index = end_index[0]
+        for channel_no in range(no_channels):
+            epoched_data[channel_no][emotion_no][0:(end_index - start_index)] = data[channel_no][start_index:end_index]
+    epoched_data = np.asarray(epoched_data, dtype=np.float32)
 
-    print(epoched_data.shape)
 
     """
+    # Visualization
     x_right = 10
     # Raw data
     plt.subplot(3, 1, 1)
--- test2.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-a = np.zeros((2, 3, 4))
-# epoched_data = np.zeros(no_channels, N, T_max*fs)
-print(a.shape)
-print(a)
-x = len(a)
-print(type(x))
